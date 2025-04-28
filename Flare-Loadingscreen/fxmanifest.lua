cc03588e2ae9010f6ebef36e602552c2da3e5a3c
Dev-Leon2025 <leonleu29@gmail.com>
2025-04-28 21:08:30 +0200

    Add files via upload

---
+++ Flare-Loadingscreen/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Flare-Scripts'
+description 'Modern FiveM Loadingscreen'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/css/config.css',
+    'html/js/app.js',
+    'html/js/config.js',
+    'html/assets/**/*'
+}
+
+client_script 'client.lua'
